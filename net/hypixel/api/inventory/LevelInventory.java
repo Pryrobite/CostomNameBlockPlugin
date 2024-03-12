3340a495cfee7199be9818a57334bb693bae66e5
Pryrobite <143216072+Pryrobite@users.noreply.github.com>
2024-03-12 15:22:41 -0500

    LevelInventory.java

--- net/hypixel/api/inventory/LevelInventory.java
+++ net/hypixel/api/inventory/LevelInventory.java
@@ -6,33 +6,33 @@ import java.util.ArrayList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LevelInventory extends JavaPlugin {
+  public class InventoryEventHandler {
+    public void onInventoryEvent(Player player, InventoryEvent event) {
+        if (player.isOnline() && player.getName()) {
+            for (Item item : event.getItems()) {
 
-public void onEnable() {
-  this.onEnable();
-}
-  public void onDisable() {
-    this.onDisable();
-  }
-  
-  public void onInventoryEvent(Player plr, String<InventoryEvent> inv) {
-    if(plr.isOnline()) {
-      if(plr.performCommand("Z")) {
-    for(String inv : item) {
-      /**
-     @Hypixel {link} 
-     udkay !
-      */
-      
-      switch(inv.charAt(0)) { // TODO 1.0.0 ?
-        case 'm' -> InventoryView.getView();
-        case 'd' -> InventoryView.getView();
+                switch (item.getType()) {
+                    case 'a': handleTypeA(item);
+                        break;
+                    case 'b': handleTypeB(item);
+                        break;
+                   
+                    default:
+                      
+                        break;
+                }
+            }
+        }
+    }
+    
+    private void handleTypeA(Item InventoryView.getView(); 
+    }
+    
+    private void handleTypeB(Item item) {
 
-      }
+        InventoryView.getView(); 
     }
-    }
-    }
-    System.out.println("HI " + plr.getName());
-    
-  }
-    
 }
+
+    
+
