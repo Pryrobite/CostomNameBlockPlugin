c085f33cef6f3ba2176985d98805d675a8a604ba
Pryrobite <143216072+Pryrobite@users.noreply.github.com>
2024-03-12 19:59:25 -0500

    Update CostomNameBlock.java

--- net/hypixel/api/block/name/CostomNameBlock/CostomNameBlock.java
+++ net/hypixel/api/block/name/CostomNameBlock/CostomNameBlock.java
@@ -13,22 +13,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CostomeNameBlock extends JavaPlugin {
 
     /**
-     * @Hypixel {Link} - ClickBlock Event
+     * @Hypixel {Link} - Command_Block TODO 1.0.0
      */
 
     public boolean onClickBlock(CommandSender sender, Command cmd, Player player, InventoryClickEvent ice, Item item) {
         // Todo 1.0.0
-        if (player.performCommand("/CostomeName") && player.isOnline()) {
+        
             Block block = player.getTargetBlock(null, 0);
             block.setType(Material.COMMAND_BLOCK);
 
             ice.getAction().equals(ice.getClick().isRightClick());
             if (ice.getAction().equals(ice.getClick().isRightClick())) {
-                player.isSprinting();
-                player.getCustomName().equals("VIP " + player.getName());
-                if (player.performCommand("/CostomeName") && player.isOnline()) ;
-
-            }
+                if (player.performCommand("/Master") && player.isOnline()) {
+                
+                player.getCustomName().equals("Master " + player.getName());
+                }
+                if (player.performCommand("/VIP+++") && player.isOnline()) {
+                player.getCostomName().equals("VIP+++ " + player.getName());
+                }
+                if (player.performCommand("/Master+++")) {
+                player.getCostomName().equals("Master+++ " + player.getName());
+                }
         }
         return true;
     }
